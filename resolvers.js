ef825ba1781a52026cb8f1bd7e78084e8998b65d
Nicholas Ko <mailnickko@gmail.com>
2018-09-27 01:37:48 -0700

    (feat) add first mutation and convert to use apollo v2

---
+++ resolvers.js
@@ -0,0 +1,19 @@
+exports.resolvers = {
+  Query: {
+    getAllRecipes: () => {}
+  },
+
+  Mutation: {
+    //parent, args, context
+    addRecipe: async (root, { name, description, category, instructions, username }, { Recipe }) => {
+      const newRecipe = await new Recipe({
+        name,
+        description,
+        category,
+        instructions,
+        username
+      }).save();
+      return newRecipe;
+    }
+  }
+};
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config({ path: './variables.env' });
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
 
+// GraphQL middleware
+const { ApolloServer } = require('apollo-server-express');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+
+// Schemas
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
 const app = express();
+// Connect Schemas with GraphQL
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    Recipe,
+    User
+  }
+});
+server.applyMiddleware({ app, path: '/graphql' });
 
 mongoose
   .connect(process.env.MONGO_URI)
