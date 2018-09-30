d1e427c4c3bf2775f54a6530274f0c730f90dfd5
Nicholas Ko <mailnickko@gmail.com>
2018-09-30 07:57:20 -0700

    (feat) include initial routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+
+import './index.css';
+import App from './components/App';
+import Signup from './components/Auth/Signup';
+import Signin from './components/Auth/Signin';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
 });
 
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/signin" component={Signin} />
+      <Route path="/signup" component={Signup} />
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+);
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Root />
   </ApolloProvider>,
   document.getElementById('root')
 );
